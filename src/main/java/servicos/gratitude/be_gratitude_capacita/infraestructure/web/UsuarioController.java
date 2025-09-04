814d4d028b0c2fbe70805b077f750f530468920f
MatheusRequena <matheus.requena@sptech.school>
2025-09-04 17:27:08 -0300

    Finalizando Controller de usuario

--- src/main/java/servicos/gratitude/be_gratitude_capacita/infraestructure/web/UsuarioController.java
+++ src/main/java/servicos/gratitude/be_gratitude_capacita/infraestructure/web/UsuarioController.java
@@ -4,11 +4,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+import servicos.gratitude.be_gratitude_capacita.core.application.command.usuario.AtualizarSenhaCommand;
 import servicos.gratitude.be_gratitude_capacita.core.application.command.usuario.CriarUsuarioCommand;
 import servicos.gratitude.be_gratitude_capacita.core.application.exception.ConflitoException;
-import servicos.gratitude.be_gratitude_capacita.core.application.usecase.usuario.CriarUsuarioUseCase;
-import servicos.gratitude.be_gratitude_capacita.core.application.usecase.usuario.ListarUsuariosUseCase;
-import servicos.gratitude.be_gratitude_capacita.core.application.usecase.usuario.PesquisarPorNomeUseCase;
+import servicos.gratitude.be_gratitude_capacita.core.application.exception.NaoEncontradoException;
+import servicos.gratitude.be_gratitude_capacita.core.application.usecase.usuario.*;
 import servicos.gratitude.be_gratitude_capacita.core.domain.Usuario;
 import servicos.gratitude.be_gratitude_capacita.infraestructure.persistence.entity.UsuarioEntity;
 
@@ -20,10 +20,20 @@ public class UsuarioController {
 
     private final CriarUsuarioUseCase criarUsuarioUseCase;
     private final ListarUsuariosUseCase listarUsuariosUseCase;
+    private final PesquisarPorNomeUseCase pesquisarPorNomeUseCase;
+    private final DeletarUsuarioUseCase deletarUsuarioUseCase;
+    private final AtualizarAcessoUseCase atualizarAcessoUseCase;
+    private final BuscarUsuarioPorIdUseCase buscarUsuarioPorIdUseCase;
+    private final AtualizarSenhaUseCase atualizarSenhaUseCase;
 
-    public UsuarioController(CriarUsuarioUseCase criarUsuarioUseCase, ListarUsuariosUseCase listarUsuariosUseCase) {
+    public UsuarioController(CriarUsuarioUseCase criarUsuarioUseCase, ListarUsuariosUseCase listarUsuariosUseCase, PesquisarPorNomeUseCase pesquisarPorNomeUseCase, DeletarUsuarioUseCase deletarUsuarioUseCase, AtualizarAcessoUseCase atualizarAcessoUseCase, BuscarUsuarioPorIdUseCase buscarUsuarioPorIdUseCase, AtualizarSenhaUseCase atualizarSenhaUseCase) {
         this.criarUsuarioUseCase = criarUsuarioUseCase;
         this.listarUsuariosUseCase = listarUsuariosUseCase;
+        this.pesquisarPorNomeUseCase = pesquisarPorNomeUseCase;
+        this.deletarUsuarioUseCase = deletarUsuarioUseCase;
+        this.atualizarAcessoUseCase = atualizarAcessoUseCase;
+        this.buscarUsuarioPorIdUseCase = buscarUsuarioPorIdUseCase;
+        this.atualizarSenhaUseCase = atualizarSenhaUseCase;
     }
 
     @PostMapping
@@ -48,5 +58,72 @@ public class UsuarioController {
         }
 
         return ResponseEntity.status(HttpStatus.OK).body(responses);
+    }
+
+    @GetMapping("/pesquisa-por-nome")
+    public ResponseEntity<List<Usuario>> pesquisarPorNome(
+            @RequestParam String nome
+    ){
+        List<Usuario> responses = pesquisarPorNomeUseCase.execute(nome);
+
+        if (responses.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(responses);
+    }
+
+    @DeleteMapping("/{idUsuario}")
+    public ResponseEntity deletarUsuario(
+            @PathVariable Integer idUsuario
+    ){
+        try {
+            deletarUsuarioUseCase.execute(idUsuario);
+            return ResponseEntity.status(HttpStatus.OK).build();
+        } catch (NaoEncontradoException e){
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, e.getMessage(), e
+            );
+        }
+    }
+
+    @PutMapping("/acesso/{idUsuario}")
+    public ResponseEntity<Usuario> atualizarUltimoAcesso(
+            @PathVariable Integer idUsuario
+    ){
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(atualizarAcessoUseCase.execute(idUsuario));
+        } catch (NaoEncontradoException e){
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, e.getMessage(), e
+            );
+        }
+    }
+
+    @GetMapping("/{idUsuario}")
+    public ResponseEntity<Usuario> encontrarUsuario(
+            @PathVariable Integer idUsuario
+    ){
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(buscarUsuarioPorIdUseCase.execute(idUsuario));
+        } catch (NaoEncontradoException e){
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, e.getMessage(), e
+            );
+        }
+    }
+
+    @PutMapping("/senha/{idUsuario}")
+    public ResponseEntity<Usuario> atualizarSenha(
+            @RequestBody AtualizarSenhaCommand request,
+            @PathVariable Integer idUsuario
+    ){
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(atualizarSenhaUseCase.execute(request, idUsuario));
+        } catch (NaoEncontradoException e){
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, e.getMessage(), e
+            );
+        }
     }
 }
