34700c27a5d0253ec3b0b759d2540275c591359f
AlexandreVNJR <alexandre.nichiojr@sptech.school>
2025-10-27 23:30:14 -0300

    send s3

--- src/main/java/servicos/gratitude/be_gratitude_capacita/BeGratitudeCapacitaApplication.java
+++ src/main/java/servicos/gratitude/be_gratitude_capacita/BeGratitudeCapacitaApplication.java
@@ -2,12 +2,21 @@ package servicos.gratitude.be_gratitude_capacita;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import jakarta.annotation.PostConstruct;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class BeGratitudeCapacitaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BeGratitudeCapacitaApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init() {
+		// Define timezone padrão da JVM para America/Sao_Paulo (Horário de Brasília)
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
 	}
 
 }
